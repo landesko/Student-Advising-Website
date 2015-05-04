8690a0650295892969f27260166371c2a2b5f4b8
rickover <dalearn1@umbc.edu>
2015-05-04 11:03:40 -0400

    SAFE modify avail page up

---
+++ testMaster/modifyAdvisorAvail.php
@@ -0,0 +1,119 @@
+<?php
+
+session_start();
+?>
+
+<html>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+
+<head>
+<title>COEIT Advisor Home Page</title>
+<!-- ============================================================== -->
+<meta name="resource-type" content="document" />
+<meta name="distribution" content="global" />
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1" />
+<meta http-equiv="Content-Language" content="en-us" />
+<meta name="description" content="UMBC Advising" />
+<meta name="keywords" content="UMBC, Advising" />
+<!-- ============================================================== -->
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
+
+
+    <!-- -->
+    <link rel="stylesheet" href="https://cdn.rawgit.com/oneyoung/jquery-calendar/master/css/style.css" />
+   <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script type="text/javascript" src="https://cdn.rawgit.com/oneyoung/jquery-calendar/master/js/calendar.js"></script>
+    
+    <!-- Custom style for sign in -->
+  <link href="css/signin.css" rel="stylesheet">
+
+   <!-- Main Style -->
+  <link href="css/main.css" rel="stylesheet">
+
+   <!-- Timetable Style -->
+  <link href="css/timetable.css" rel="stylesheet">
+    
+    <link rel="icon" type="image/png" href="icon.png" />
+    
+   
+</head>
+
+
+<body>
+
+<!--Navigation Bar-->
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <img class="navbar-brand"  src="res/logo.png" >      
+        </div>
+        <ul class="nav navbar-nav navbar-right">
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Menu <span class="caret"></span></a>
+          <ul class="dropdown-menu" role="menu">
+            <li><a href="advisorSetAvail.php">Set Availability</a></li>
+            <li class="divider"></li>
+            <li><a href="advisorHome.php">Home</a></li>
+            <li class="divider"></li>
+            <li><a href="advisorIndex.php">Log Out</a></li>
+          </ul>
+        </li>
+      </ul>
+      <div class="titleBar">
+             <h2>COEIT Advisor Home Page</h2>
+      </div>
+       
+    </div>
+  </nav>
+
+<div class="container">
+<div class="tableBar">
+<?php
+echo("<html><head></head><body>");
+
+$debug = false;
+include('CommonMethods.php');
+$COMMON = new Common($debug); // common methods
+//var_dump($_POST);
+
+
+
+echo("<p>Advisor Availability Updated</p>");
+echo("<form action='modifySetAvail.php' method='post' name='advAvail'>");
+echo("<button class='btn btn-sm btn-primary' type='submit' >Set More Availabilities</button>");
+echo("</form>");
+
+
+echo("<form action='advisorHome.php' method='post' name='advAvail'>");
+echo("<button class='btn btn-sm btn-primary' type='submit' >Back To Advisor Home</button>");
+echo("</form>");
+?>
+
+</div>
+</div>
+
+<!-- Load javascript required for Bootstrap animation-->
+<script src="https://code.jquery.com/jquery.js"></script>
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+
+<script>
+$(document).ready(function(){
+		$(".advisorSlotOpen").click(function(){
+			console.log("clicked");
+		});
+});
+</script>
+
+</body>
+</html>
